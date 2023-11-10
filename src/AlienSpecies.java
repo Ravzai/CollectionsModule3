2dbbb76f0f555965b14489d55c266bfddc4ddee5
ravzai <rinanc@live.nl>
2023-11-10 12:27:54 +0100

    Initial commit

---
+++ src/AlienSpecies.java
@@ -0,0 +1,13 @@
+class AlienSpecies {
+    private String name;
+    private String homePlanet;
+    private String specialAbility;
+
+    public AlienSpecies(String name, String homePlanet, String specialAbility) {
+        this.name = name;
+        this.homePlanet = homePlanet;
+        this.specialAbility = specialAbility;
+    }
+
+    // Getters for properties
+}
